d19d557fd2070c73df8fe00fed4e27dfb3adc8bb
notharsh0905 <harsh0905v@gmail.com>
2026-07-21 21:58:36 +0530

    ...

---
+++ 61.py
@@ -0,0 +1,10 @@
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+merged = []
+for item in list1:
+    merged.append(item)
+for item in list2:
+    merged.append(item)
+
+print(merged)  # Output: [1, 2, 3, 4, 5, 6]   
