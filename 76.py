8bc0b17ec99ad44ea80e6ed3e9149c10422a0478
notharsh0905 <harsh0905v@gmail.com>
2026-08-23 23:31:17 +0530

    Add leap year checker script

--- 76.py
+++ 76.py
@@ -1,16 +1,7 @@
-#Write a program to print all the unique combinations of 1,2,3 and 4
-"""from itertools import permutations
+#Given a year, determine if it is a leap year.
+#Leap year rules: divisible by 4, except for centuries which must be divisible by 400.
+#Example: 2020 -> True, 1900 -> False, 2000 -> True
 
-nums = [1, 2, 3, 4]
-
-for p in permutations(nums):
-    print(*p)"""
-
-nums = [1, 2, 3, 4]
-
-for i in nums:
-    for j in nums:
-        for k in nums:
-            for l in nums:
-                if i != j and i != k and i != l and j != k and j != l and k != l:
-                    print(i, j, k, l)
+year = int(input("Enter a year: "))
+is_leap = (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+print(f"{year} is a leap year: {is_leap}")
